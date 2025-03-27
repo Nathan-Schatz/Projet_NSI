bdf8100ee4b302e2ae22d40c522eda33836250c9
Soso.madr <soren.madier@gmail.com>
2025-03-27 09:24:37 +0100

    natahn demerde toi mtn

--- inscription.php
+++ inscription.php
@@ -29,7 +29,6 @@ if(isset($_POST['envoi'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style7.css">
     <title>Inscription</title>
 </head>
@@ -60,6 +59,5 @@ if(isset($_POST['envoi'])){
                 </div>
         </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
