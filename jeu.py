ac39b539581d65cd4b9fef1eb54998bbaccb478f
paulptj <petitjean.paul208@gmail.com>
2025-03-31 11:30:57 +0200

    jeu presque abouti version brouillon

--- jeu.py
+++ jeu.py
@@ -5,6 +5,9 @@ pyxel.init (128,128,"neon nexus",quit_key=pyxel.KEY_ESCAPE)
 e_x=60
 e_y=60
 tir_liste =[]
+ennemi_liste=[]
+vie=3
+point=0
 
 
 
@@ -34,39 +37,65 @@ def tir_deplacement (tir_liste) :
     if tir [1] <- 8:
       tir_liste.remove(tir) 
   return(tir_liste)
-
-def ennemi_creation (x,y,ennemi_liste):
-  if (pyxel.frame_count % 30 == 0):
-    ennemi_liste.append([random(4,124),0])
+  
+def ennemi_creation (ennemi_liste):
+  if (pyxel.frame_count % 50 == 0):
+    ennemi_liste.append([random.randint(0,120),0])
   return (ennemi_liste)
 
 def ennemi_mouvement (ennemi_liste) :
   for position in ennemi_liste:
     position[1] +=1
-    if position[1] >-1 :
+    if position[1] >128:
       ennemi_liste.remove(position)
   return (ennemi_liste)
 
-  
+def suppresion_ennemi ():
+    for position in ennemi_liste:
+      for tir in tir_liste :
+        if position [0] <= tir[0]+1  and tir[1]+8 >= position[1] and position[1]+8 >= tir[1]+8 :
+          tir_liste.remove(tir)
+          ennemi_liste.remove(position)
+          point+=1
+    return(ennemi_liste,tir_liste)
+
+def vaisseau_suppresion (vie):
+  for position in ennemi_liste :
+    if position[0] <= e_x+4 and position[1] <= e_y+4 and position[0]+4 >= e_x and position[1]+4 >= e_y:
+      ennemi_liste.remove(position)
+      vie-=1
+  return (vie)
 
 
 
 def update () :
-  global e_x ,e_y ,tir_liste
+  global e_x ,e_y ,tir_liste,ennemi_liste,vie
   e_x,e_y=e_deplacement(e_x,e_y)
   tir_liste=tirs_creation(e_x,e_y,tir_liste)
   tir_liste=tir_deplacement(tir_liste)
   ennemi_liste=ennemi_creation(ennemi_liste)
   ennemi_liste=ennemi_mouvement(ennemi_liste)
+  suppresion_ennemi()
+  vie=vaisseau_suppresion(vie)
   
 
 
 def draw ():
   pyxel.cls(0)
-  pyxel.rect(e_x,e_y,4,4,4)
-  
-  for tir in tir_liste:
-    pyxel.rect(tir[0],tir[1],1,4,11)
+  if vie >0 :
+    pyxel.rect(e_x,e_y,4,4,4)
+    for tir in tir_liste:
+      pyxel.rect(tir[0],tir[1],1,4,11)
+
+    for position in ennemi_liste :
+      pyxel.rect(position[0],position[1],4,4,12)
+    
+    pyxel.text(10,10,"Vie"+str(vie),10)
+    
+    pyxel.text(10,10,str(point),10)
+  else:
+    pyxel.text(10,50,"Perdu retourne au lobby",8)
+
 
  
 
